65525813a5bfc61331743e7ce2801ad4c489fda3
EmanuelLop <utp0009948@alumno.utpuebla.edu.mx>
2022-07-12 19:49:48 -0500

    Archivos de navegacion

---
+++ application/views/diseño/footer.php
@@ -0,0 +1,23 @@
+<footer class="ng-scope ">
+    <div class="jumbotron " style="margin-bottom: 0%">
+        <!-- Example row of columns -->
+        <center><div class="row">
+          
+          <div class="col" >
+            <h2>CONTACTO</h2>
+            <p align="center"> Bienvenido a Gamer</p>
+            <p class="phone">Teléfono: +52 2226526858</p>
+            <p class="chat"><a href="https://web.whatsapp.com/" target="_blank"><img src="img/WS.png" width="40" height="40"  alt="fb" >Whatsapp Web</a></p>
+            
+          </div>
+           
+        
+        <div class="col">
+           <img class="d-block w-50" src="img/slogan.png" alt="pastel">
+        </div>
+        </div>
+        </center>
+        <a class="btn btn-secondary" href="index.php/Login/login" role="button">Panel</a>
+      
+    </div> <!-- /container -->
+</footer>
---
+++ application/views/diseño/navbar.php
@@ -0,0 +1,47 @@
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" class=href="img/logo_2.png">
+      <img class="d-block w-100" src="img/bg5.gif" alt="gamer" >
+      <link rel="stylesheet" href="dist/estilo.css">
+
+      <title>GAMER</title>
+  </head>
+
+      <nav class="navbar navbar-expand-md navbar-dark justify-content-between" style="background-color: rgb(198, 46, 95);">
+        <a class="navbar-brand " href="index.php/"><img src="img/logo_2.png" width="50" height="50"  alt="gamer">Inicio</a>
+    
+       <button class="navbar-toggler collapsed navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navarCollapse" aria-controls="navarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+    <div class="navbar-collapse collapse" id="navarCollapse">
+          <ul class="navbar-nav  mr-auto">
+            
+            <li class="nav-item">
+                
+                <a class="nav-link btn btn-light text-dark" style="margin: 10px; " href="index.php/Login/login/">Sucursales</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn btn-light text-dark" style="margin: 10px;" href="index.php/Productos/producto/">Productos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn btn-light text-dark" style="margin: 10px;" href="index.php/Tienda/tienda/">Registro</a>
+            </li>
+          </ul>
+
+          <!--  No jala aqui va repositorio de imagenes de tienda 
+            <div>
+                        <form action="" method="get"> 
+                        <input type="search" name="q" placeholder="Busca lo que te guste aqui" required="required" >
+                        <button type="submit" class="btn btn-primary float-right"><i class="fas fa-plus"></i> Buscar</button>
+            </div> -->
+
+      </div>
+   <!--   <form class="form-inline">
+
+         <a href="index.php/usuarios/cerrar_sesion" class="btn btn-outline-primary my-2 my-sm-0" role="button" aria-pressed="true">Cerrar Sesion</a>
+      </form>-->
+   </nav>   
